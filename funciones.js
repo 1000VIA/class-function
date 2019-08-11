bc31551d3fd43b819e69721d5d9c8a62221f2abf
cyfco-02 <milvampa1@gmail.com>
2019-08-11 11:41:34 -0500

    Ejemplos de funciones

---
+++ funciones.js
@@ -0,0 +1,24 @@
+let n1 = 2;
+let n2 = 3;
+let n3 = 4;
+let n4 = 5;
+
+function sumar(n1, n2){
+return `Mi primer suma ${n1 + n2}`;
+}
+let result = sumar(n1, n2); //Capturo la variable
+
+console.log( sumar(n1, n2));
+console.log(result);
+
+
+sumar(n1, n2);
+sumar(n1, n3);
+sumar(n1, n4);
+// console.log(`Mi primer suma ${n1 + n2}`);
+
+// console.log(`Mi segundo suma ${n1 +  n3}`);
+
+// console.log(`Mi tercer suma ${n1 +  n4}`);
+
+
